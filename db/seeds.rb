980a85794efc88e941ec2d19f106e438a005a1ed
Andre Brissette <andre.brissette@jestadigital.com>
2013-05-10 16:21:31 -0400

    WIP : fix of some test for type addition

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,4 +10,27 @@ projects = Project.create([ {code: '603002', name: 'iLove Server Design'}, \
                             {code: '603008', name: 'Mobile Design (Android & iOS)'}, \
                             {code: '603009', name: 'Mobile Development (Android & iOS)'}])
 
-Worklog.create(date:'01/05/2013', description:'Make some work', employee_name:'Andre', project: projects.first, type_code:'4',value:'30')
+types = Type.create([ {code: '7000', name: 'Project Mng'}, \
+                            {code: '7130', name: 'Tech Spec - Requirements'}, \
+                            {code: '7210', name: 'Design Code'}, \
+                            {code: '7100', name: 'User Interface Design'}, \
+                            {code: '8310', name: 'Issue/Bug Analysis'}, \
+                            {code: '7230', name: 'Programming'}, \
+                            {code: '7240', name: 'Code Review/Pull Request'}, \
+                            {code: '7414', name: 'Deployment'}, \
+                            {code: '9925', name: 'Recruiting'}, \
+                            {code: '9990', name: 'Vacation'}, \
+                            {code: '9960', name: 'Travel Non-Billable'}, \
+                            {code: '9910', name: 'Internal training'}, \
+                            {code: '9940', name: 'Staff Meeting'}, \
+                            {code: '7050', name: 'Ressource/Sprint Planning'}, \
+                            {code: '7140', name: 'Design Test Case'}, \
+                            {code: '7250', name: 'Unit Testing'}, \
+                            {code: '7520', name: 'Integrated Testing'}, \
+                            {code: '7530', name: 'Performance Testing'}, \
+                            {code: '9993', name: 'Absence (Unpaid)'}, \
+                            {code: '9991', name: 'Illness'}, \
+                            {code: '9964', name: 'Travel Arrangements'}, \
+                            {code: '9935', name: 'Staff Management'}])
+
+Worklog.create(date:'01/05/2013', description:'Make some work', employee_name:'Andre', project: projects.first, type: types.first,value:'30')
--- test/unit/worklog_test.rb
+++ test/unit/worklog_test.rb
@@ -8,13 +8,13 @@ class WorklogTest < ActiveSupport::TestCase
     assert worklog.errors[:date].any?
     assert worklog.errors[:employee_name].any?
     assert worklog.errors[:project].any?
-    assert worklog.errors[:type_code].any?
+    assert worklog.errors[:type].any?
     assert worklog.errors[:value].any?
   end
 
   test "time logged must be at least 30 min" do
-    worklog = worklogs(:worklog_on_ilove)
-    worklog.value = 28
+    worklog = worklogs(:requirement_on_ilove)
+    worklog = 28
 
     assert worklog.invalid?
     assert worklog.errors[:value].any?
@@ -22,11 +22,19 @@ class WorklogTest < ActiveSupport::TestCase
   end
 
   test "project should be valid" do
+    worklog = worklogs(:requirement_on_ilove)
+    worklog.project = nil
 
+    assert worklog.invalid?
+    assert worklog.errors[:project].any?
   end
 
   test "work type should be valid" do
+    worklog = worklogs(:requirement_on_ilove)
+    worklog.type = nil
 
+    assert worklog.invalid?
+    assert worklog.errors[:type].any?
   end
 
 end
