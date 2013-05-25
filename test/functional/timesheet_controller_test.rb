3cd39d5143b9bfa804288713cd1640f2638b05ec
Andre Brissette <andre.brissette@jestadigital.com>
2013-05-25 10:17:03 -0400

    WIP: addning worklog partial

--- test/functional/timesheet_controller_test.rb
+++ test/functional/timesheet_controller_test.rb
@@ -5,5 +5,9 @@ class TimesheetControllerTest < ActionController::TestCase
     get :index
     assert_response :success
   end
-
+  context "when viewing a timesheet" do
+    should "see all his worklogs within separate form to edit" do
+      assert false
+    end
+  end
 end
